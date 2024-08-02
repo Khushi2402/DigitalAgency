da02d8dd9e6c749f0a32325e68d904f4107c03d1
Khushi Kesharwani <khushibharatkesharwani@gmail.com>
2024-08-02 15:28:05 +0530

    feat: Add Pricing Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import Product from './components/Product';
 import Feature from './components/Feature';
+import Pricing from './components/Pricing';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -16,8 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
           <Route path="/feature" element={<Feature />} />
-          {/* 
           <Route path="/pricing" element={<Pricing />} />
+          {/*
           <Route path="/contact" element={<ContactUs />} /> */}
         </Routes>
         <Footer />
